c663469647a70fe6103db0b1366fe24e9876cc0e
Zhayvoronok Kateryna <1gayv425@gmail.com>
2024-03-19 20:28:09 +0200

    added clues functional

--- src/App.js
+++ src/App.js
@@ -20,52 +20,10 @@ function App() {
   const allCityJson = cities;
 
 
-  // React.useEffect(() => {
-  //   const fetchData = async () => {
-  //     setIsLoading(true);
-  
-  //     try {
-  //       if (!city) {
-  //         setIsNotValid(true);
-  //         return;
-  //       }
-  //       const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=6557810176c36fac5f0db536711a6c52`);
-  //       const json = await response.json();
-  //       setData(json);
-  //       if (json && json.list && json.list.length > 0) {
-  //         const nextDates = nextDays();
-  //         setDates(nextDates);
-  //       }
-  //     } catch (error) {
-  //       console.warn(error);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   };
-  
-  //   const fetchGEOData = async () => {
-  //     try {
-  //       setIsLoading(true);
-  //       const response = await fetch(`https://ipapi.co/json`);
-  //       const json = await response.json();
-  //       setCity(json.city);
-  //     } catch (error) {
-  //       console.warn(error);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   }
-  
-  //   if (city) {
-  //     fetchData();
-  //   } else {
-  //     fetchGEOData();
-  //   }
-  // }, [city]);
   React.useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-
+  
       try {
         if (!city) {
           setIsNotValid(true);
@@ -80,12 +38,11 @@ function App() {
         }
       } catch (error) {
         console.warn(error);
-        setIsNotValid(true); // Обработка ошибки, установка isNotValid в true
       } finally {
         setIsLoading(false);
       }
     };
-
+  
     const fetchGEOData = async () => {
       try {
         setIsLoading(true);
@@ -94,68 +51,55 @@ function App() {
         setCity(json.city);
       } catch (error) {
         console.warn(error);
-        setIsNotValid(true); // Обработка ошибки, установка isNotValid в true
       } finally {
         setIsLoading(false);
       }
-    };
-
-    fetchData();
-    fetchGEOData();
+    }
+  
+    if (city) {
+      fetchData();
+    } else {
+      fetchGEOData();
+    }
   }, [city]);
 
-  // const handleSumbit = async (e) => {
-  //   if (e.key === 'Enter') {
-  //     const inputCity = e.target.value;
-  //     setIsLoading(true);
-  //     try {
-  //       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&APPID=6557810176c36fac5f0db536711a6c52`);
-  //       const json = await response.json();
-  //       if (json.cod === 200) {
-  //         setCity(inputCity);
-  //         setIsNotValid(false);
-  //       } else {
-  //         setIsNotValid(true);
-  //       }
-  //     } catch (error) {
-  //       console.warn(error);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //     document.querySelector('input').value = '';
-  //   }
-  // }
-  
 
-  // const clueChange = (e) => {
-  //   const inputCurrent = e.target.value.toLowerCase().trim();
-  //   const arrClue = allCityJson
-  //   .flatMap(arr => arr)
-  //   .filter(obj => obj.name.toLowerCase().includes(inputCurrent))
-  //   .map(obj => obj.name);
-  //   setClues(arrClue);
-  //   console.log(clues);
-  // }
-  const clueChange = (e) => {
-    setInputValue(e.target.value.toLowerCase().trim());
-  };
-
-  useEffect(() => {
-    const filteredClues = allCityJson
-      .flatMap(arr => arr)
-      .filter(obj => obj.name.toLowerCase().includes(inputValue))
-      .map(obj => obj.name);
-    setClues(filteredClues);
-  }, [inputValue, allCityJson]);
-
-  const handleSumbit = (e) => {
+  const handleSumbit = async (e) => {
     if (e.key === 'Enter') {
-      setCity(inputValue);
-      setInputValue('');
-      setIsNotValid(false);
+      const inputCity = e.target.value;
+      setIsLoading(true);
+      try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&APPID=6557810176c36fac5f0db536711a6c52`);
+        const json = await response.json();
+        if (json.cod === 200) {
+          setCity(inputCity);
+          setIsNotValid(false);
+        } else {
+          setIsNotValid(true);
+        }
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setIsLoading(false);
+      }
+      document.querySelector('input').value = '';
     }
-  };
+  }
   
+
+  const clueChange = (e) => {
+    const inputCurrent = e.target.value.toLowerCase().trim();
+    
+    if (inputCurrent.length >= 3) {
+        const arrClue = allCityJson
+            .flatMap(arr => arr)
+            .filter(obj => obj.name.toLowerCase().includes(inputCurrent))
+            .map(obj => obj.name);
+        setClues(arrClue);
+    } else {
+        setClues([]);
+    }
+}
 
   const getNextDate = (daysToAdd) => {
     const newDate = new Date(date);
@@ -172,7 +116,11 @@ function App() {
     return dates;
   }
 
-
+  const handleClueClick = (city) => {
+    setCity(city);
+    document.querySelector('input').value = '';
+    setClues([]);
+  };
   
 
 
@@ -180,17 +128,17 @@ function App() {
     <div className="App">
       {isLoading ? (
         <>
-        <Input handleSumbit={handleSumbit} clueChange={clueChange} clues={clues}/>
+        <Input handleSumbit={handleSumbit} clueChange={clueChange} clues={clues} handleClueClick={handleClueClick}/>
           <Skeleton/>
         </>
       ) : isNotValid ? (
         <>
-        <Input handleSumbit={handleSumbit} clueChange={clueChange} clues={clues}/>
+        <Input handleSumbit={handleSumbit} clueChange={clueChange} clues={clues} handleClueClick={handleClueClick}/>
           <div className='wrapper-intro-box'><MainIntro /></div>
         </>
       ) : (
         <>
-          <Input handleSumbit={handleSumbit} clueChange={clueChange} clues={clues}/>
+          <Input handleSumbit={handleSumbit} clueChange={clueChange} clues={clues} handleClueClick={handleClueClick}/>
           <div className='weather-block'>
             <Wrapper name={data === '' ? '' : (data.city ? data.city.name : '')}
               photo={data === '' ? '' : (data.list && data.list.length > 0 ? data.list[0].weather[0].icon : '')}
--- src/Component/Input.jsx
+++ src/Component/Input.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-export const Input = ({handleSumbit, clueChange, clues}) => {
+export const Input = ({handleSumbit, clueChange, clues, handleClueClick}) => {
+  const firstFiveClues = clues.slice(0, 3);
+  const renderedClues = firstFiveClues.map((city, index) => (
+    <span key={index} onClick={() => handleClueClick(city)}>{`${city} ${index === 2 ? '' : ', '}`}</span>
+  ));
+
   return (
     <div className='inputBox'>
-        <input className='input' type='text' placeholder='Input your city' onKeyDown={(e) => handleSumbit(e)} onChange={(e) => clueChange(e)}/>
-    <p>{clues}</p>
-  </div>
-    )
-}
+      <input className='input' type='text' placeholder='Input your city' onKeyDown={(e) => handleSumbit(e)} onChange={(e) => clueChange(e)}/>
+      <p>{clues.length !== 0 ? `Maybe you meant:` : ''} {renderedClues}</p>
+    </div>
+  );
+};
